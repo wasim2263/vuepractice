95f94609692da0ddc7e45323eaebab8b03651d33
no-PC\no <abdullahwasim42@gmail.com>
2017-03-05 22:28:16 +0600

    ok

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ var app = new Vue({
         message: 'Hello Vue!'
     }
 })
+var vm = new Vue({
+    el: '#example',
+    data: {
+        message: 'Hello'
+    },
+    computed: {
+        // a computed getter
+        reversedMessage: function () {
+            // `this` points to the vm instance
+            return this.message.split('').reverse().join('')
+        }
+    }
+})
